b16b3f99a9577b35620976d5ac9e670319a1700a
Leshly Ontiveros <85622591+leshlyontiveros@users.noreply.github.com>
2022-12-15 23:49:52 -0300

    projeto

---
+++ pages/checkout/successfulorder.page.tsx
@@ -0,0 +1,75 @@
+import { CardMedia, Container, Typography } from "@mui/material";
+import { useCheckoutState } from "contexts/Context";
+import Head from "next/head";
+
+export default function SuccessFullOrder() {
+
+    const { checkout, order } = useCheckoutState()
+
+    return (
+
+        <Container>
+            <Head>
+                <title>Marvel - Aproveite sua compra!</title>
+            </Head>
+            <Container sx={{
+                width: '400px', height: '100px', margin: '15px auto', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#b01010', color: 'white', borderRadius: '35px'}}>
+                <Typography
+                    gutterBottom
+                    noWrap
+                    variant="h4"
+                    component="div"
+                >
+                    Aproveite sua compra!
+                </Typography>
+            </Container >
+
+            <Container sx={{ gap: '20px', margin: '20px', display: 'flex', flexDirection: 'column', width:"350px" }}>
+                <Typography
+                    gutterBottom
+                    noWrap
+                    variant="h5"
+                    component="div"
+                >
+                    {order?.title}
+                </Typography>
+
+                <CardMedia sx={{ borderRadius: '25px', boxShadow:"20px -12px 13px -1px rgb(151 117 245)" }}
+                    component="img"
+                    width="52%"
+                    height="250"
+                    image={`${order?.path}.${order?.extension}`}
+                    alt={order?.title}
+                />
+
+                <Typography
+                    gutterBottom
+                    noWrap
+                    variant="h5"
+                    component="div"
+                >Valor pago: R$ {order?.price}
+                </Typography>
+
+                <Typography
+                    gutterBottom
+                    noWrap
+                    variant="h5"
+                    component="div"
+                >
+                    Comprador: {checkout.name} {checkout.lastname}
+                </Typography>
+                <Typography
+                    gutterBottom
+                    noWrap
+                    variant="h5"
+                    component="div"
+                >
+                    Entregar no endereço: {checkout.address1}
+                </Typography>
+
+
+            </Container>
+
+        </Container >
+    )
+}
---
+++ pages/index.page.tsx
@@ -0,0 +1,95 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Comic } from "shared/types/apiSchema";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Button, CardMedia, Container, Link, Typography } from "@mui/material";
+import { useGetComics } from "dh-marvel/services/marvel/useGetComics";
+
+
+export default function Home() {
+
+  const { data, loadingMoreComics, loadingLessComics, isLoading } = useGetComics()
+
+
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+
+  return (
+    <Container sx={{ padding: '20px', textAlign: 'center' }}>
+      <Head>
+        <title>Marvel - Home</title>
+      </Head>
+
+      <Typography
+        gutterBottom
+        align="center"
+        noWrap
+        variant="h3"
+        component="div"
+        color=" #ff2d55"
+      >
+        Seja bem vindo ao DH Marvel
+      </Typography>
+
+
+      <Box sx={{ flexGrow: 1, margin: '20px',  }}>
+        <Grid sx={{ justifyContent: 'center' }} container spacing={2}>
+          {data?.data?.results.map((comic: Comic) => (
+            <Grid sx={{  }}  key={comic.id} width={230} item >
+              <Typography
+                gutterBottom
+                noWrap
+                variant="h5"
+                component="div"
+              >
+                {comic.title}
+              </Typography>
+              <CardMedia
+                sx={{borderRadius:"30px", boxShadow:"20px -12px 13px -1px rgb(131 21 25)"}}  
+                component="img"
+                max-width="230"
+                height="194"
+                image={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+                alt={comic.title}
+              />
+              <div style={{ display: 'flex', justifyContent: 'center', gap: 10, marginTop: 10 }}>
+                <Link href={`/checkout/${comic.id}`}>
+                  <Button variant="contained">Comprar</Button>
+                </Link>
+                <Link href={`/comic/${comic.id}`}>
+                  <Button  variant="outlined">
+                    Detalhes
+                  </Button>
+                </Link>
+
+              </div>
+
+            </Grid>
+          ))}
+
+
+        </Grid>
+      </Box>
+
+      <Button onClick={loadingLessComics} variant="outlined">
+         Carregar - 12....
+      </Button>
+      <Button sx={{ marginLeft: "12px" }} onClick={loadingMoreComics} variant="outlined">
+        Carregar + 12....
+      </Button>
+
+    </Container>
+  );
+};
+
+
